8c8181fa29ffcbcc951e7978dd11edd8459510ef
Chris <christopher.juerges@uni-wuerzburg.de>
2019-12-28 14:32:59 +0100

    added Join-method to multiintervals

--- multiintervals/multiinterval.go
+++ multiintervals/multiinterval.go
@@ -1,5 +1,7 @@
 package multiintervals
 
+import "container/list"
+
 type MultiInterval struct {
 	intervalParts []*IntervalPart
 }
@@ -32,6 +34,12 @@ func New(boundaries ...int) *MultiInterval {
 		parts[i/2] = newPart(boundaries[i], boundaries[i+1])
 	}
 
+	interval := MultiInterval{intervalParts: parts}
+
+	return &interval
+}
+
+func newInternal(parts []*IntervalPart) *MultiInterval {
 	interval := MultiInterval{intervalParts: parts}
 
 	return &interval
@@ -87,7 +95,7 @@ func (i *MultiInterval) GetPart(part int) *IntervalPart {
 }
 
 func (i *MultiInterval) Intersects(other *MultiInterval) bool {
-	if i.GetNumParts() == 0 || other.GetNumParts() == 0 {
+	if other == nil || i.GetNumParts() == 0 || other.GetNumParts() == 0 {
 		return false
 	}
 
@@ -109,6 +117,100 @@ func (i *MultiInterval) Intersects(other *MultiInterval) bool {
 			currentPartI++
 		} else {
 			currentPartOther++
+		}
+	}
+}
+
+func (i *MultiInterval) Join(other *MultiInterval) *MultiInterval {
+	if other == nil || other.GetNumParts() == 0 {
+		return i
+	}
+
+	if i.GetNumParts() == 0 {
+		return other
+	}
+
+	currentPartI := 0
+	currentPartOther := 0
+	partLst := list.New()
+	var lastJoinedPart *IntervalPart
+
+	if i.GetPart(currentPartI).start < other.GetPart(currentPartOther).start {
+		lastJoinedPart = i.GetPart(currentPartI)
+		currentPartI++
+	} else {
+		lastJoinedPart = other.GetPart(currentPartOther)
+		currentPartOther++
+	}
+
+	for {
+		if currentPartI >= i.GetNumParts() {
+			lastJoinedPart = getLastJoined(other, lastJoinedPart, currentPartOther, partLst)
+			partLst.PushBack(lastJoinedPart)
+			for j := currentPartOther + 1; j < other.GetNumParts(); j++ {
+				partLst.PushBack(other.GetPart(j))
+			}
+			break
+		}
+
+		if currentPartOther >= other.GetNumParts() {
+			lastJoinedPart = getLastJoined(i, lastJoinedPart, currentPartI, partLst)
+			partLst.PushBack(lastJoinedPart)
+			for j := currentPartI + 1; j < i.GetNumParts(); j++ {
+				partLst.PushBack(i.GetPart(j))
+			}
+			break
+		}
+
+		if i.GetPart(currentPartI).start < other.GetPart(currentPartOther).start {
+			lastJoinedPart = getLastJoined(i, lastJoinedPart, currentPartI, partLst)
+			currentPartI++
+		} else {
+			lastJoinedPart = getLastJoined(other, lastJoinedPart, currentPartOther, partLst)
+			currentPartOther++
+		}
+	}
+
+	index := 0
+	intervalParts := make([]*IntervalPart, partLst.Len())
+	for element := partLst.Front(); element != nil; element = element.Next() {
+		intervalParts[index] = element.Value.(*IntervalPart)
+		index++
+	}
+
+	return newInternal(intervalParts)
+}
+
+func getLastJoined(mi *MultiInterval, joinedPart *IntervalPart, currentPart int, partLst *list.List) *IntervalPart {
+	if mi.GetPart(currentPart).Intersects(joinedPart) || mi.GetPart(currentPart).touches(joinedPart) {
+		return mergeParts(joinedPart, mi.GetPart(currentPart))
+	} else {
+		partLst.PushBack(joinedPart)
+		return mi.GetPart(currentPart)
+	}
+}
+
+func (p *IntervalPart) touches(other *IntervalPart) bool {
+	return p.end == other.start || p.start == other.end
+}
+
+func mergeParts(part1 *IntervalPart, part2 *IntervalPart) *IntervalPart {
+	s1 := part1.start
+	s2 := part2.start
+	e1 := part1.end
+	e2 := part2.end
+
+	if s1 < s2 {
+		if e1 > e2 {
+			return newPart(s1, e1)
+		} else {
+			return newPart(s1, e2)
+		}
+	} else {
+		if e1 > e2 {
+			return newPart(s2, e1)
+		} else {
+			return newPart(s2, e2)
 		}
 	}
 }
--- multiintervals/multiinterval_test.go
+++ multiintervals/multiinterval_test.go
@@ -1,6 +1,9 @@
 package multiintervals
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestIntersects(t *testing.T) {
 	interval1 := New(10, 20, 30, 40, 50, 60)
@@ -17,5 +20,34 @@ func TestIntersects(t *testing.T) {
 
 	if !inter12 && inter13 && !inter14 && inter15 && inter51 {
 		t.Error("Intersection not working")
+	}
+}
+
+func TestJoin(t *testing.T) {
+	interval1 := New(10, 20, 30, 40)
+	interval2 := New(20, 30, 40, 50)
+	interval3 := New(41, 49)
+	interval4 := New(35, 55, 100, 200, 300, 400)
+
+	interval12 := interval1.Join(interval2)
+	interval21 := interval2.Join(interval1)
+	interval11 := interval1.Join(interval1)
+	interval13 := interval1.Join(interval3)
+	interval14 := interval1.Join(interval4)
+
+	if interval12.GetNumParts() != 1 || interval21.GetNumParts() != 1 {
+		t.Error("Interval should be one big interval after join. NumParts: " + strconv.Itoa(interval12.GetNumParts()))
+	}
+
+	if interval11.GetNumParts() != 2 {
+		t.Error("Should still be the same")
+	}
+
+	if interval13.GetNumParts() != 3 {
+		t.Error("Parts should be distinct")
+	}
+
+	if interval14.GetNumParts() != 4 {
+		t.Error("Only last part should join")
 	}
 }
